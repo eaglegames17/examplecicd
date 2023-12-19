6b56e4319fcfb3a66e759735d6e29f470fa1493f
eaglegames17 <133218802+eaglegames17@users.noreply.github.com>
2023-12-20 01:54:52 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div classname="text-red-500">This is Working...yeeeehhhh<div/>
+    <div className="text-red-500">This is Working...yeeeehhhhNoooooooooooo<div/>
   )
 }
